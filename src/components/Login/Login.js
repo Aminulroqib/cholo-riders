3ea95272b50de2c8c280a51f70003a3c0a9ec7fb
Aminul Islam <aminulislamroqib@gmail.com>
2021-06-24 14:00:22 +0600

    resubmit

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -7,15 +7,13 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import { Link } from "react-router-dom";
 import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import { FacebookLoginButton, GoogleLoginButton } from "react-social-login-buttons";
 import firebase from "firebase/app";
-import "firebase/auth";
 import firebaseConfig from './firebase.config';
+import "firebase/auth";
 import { UserContext } from '../../App';
 import { useHistory, useLocation } from 'react-router-dom';
 
@@ -41,7 +39,6 @@ const useStyles = makeStyles((theme) => ({
 
 const Login = () => {
     const [loggedInUser, setLoggedInUser] = useContext(UserContext);
-    const [newUser, setNewUser] = useState(false);
     const history = useHistory();
     const location = useLocation();
     const { from } = location.state || { from: { pathname: "/" } };
